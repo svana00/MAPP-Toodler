8565158824ef097604ac093f10a0ae4a051d7d41
svana00 <54863023+svana00@users.noreply.github.com>
2020-11-24 12:44:12 +0000

    front page update

--- src/views/Main/index.jsx
+++ src/views/Main/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import {View, Text, TouchableHighlight, Image} from 'react-native';
+import {
+  View, Text, TouchableHighlight, Image
+} from 'react-native';
 import logo from '../../resources/logo.png';
 import styles from './styles';
 
-const Main = ({ navigation: {navigate}}) => (
+const Main = ({ navigation:{navigate}}) => (
   <View style={styles.container}>
     <Image source={logo} style={styles.logo} />
-    <Text>Toodler</Text>
-    <Text>So simple a toddler could use it</Text>
+    <Text style={styles.header}>Toodler</Text>
+    <Text style={styles.paragraph}>So simple a toddler could use it</Text>
+    <TouchableHighlight style={styles.button}>
+      <Text style={styles.buttonText}>Get Started</Text>
+    </TouchableHighlight>
   </View>
 );
 
