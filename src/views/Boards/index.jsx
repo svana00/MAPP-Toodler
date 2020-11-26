ba4aee30c3e212f361f6f4cd37e987114d2fc118
svana00 <54863023+svana00@users.noreply.github.com>
2020-11-26 19:30:04 +0000

    add virkar í boards bitches

--- src/views/Boards/index.jsx
+++ src/views/Boards/index.jsx
@@ -1,69 +1,82 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import { View, Alert } from 'react-native';
 import Toolbar from '../../components/Toolbar';
 import data from '../../resources/data.json';
 import CreateBoard from '../../components/CreateBoard';
 import BoardList from '../../components/BoardList';
-import {takePhoto, selectFromCameraRoll} from '../../services/imageService';
-import { addBoard } from '../../actions/boards';
+import { takePhoto, selectFromCameraRoll } from '../../services/imageService';
 
 class Boards extends React.Component {
-  state = {
-    boards: data.boards,
-    isCreateBoardModalOpen: false,
-    thumbnailPhoto: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      boards: data.boards,
+      isCreateBoardModalOpen: false,
+      thumbnailPhoto: '',
+      nextBoardId: 3,
+    };
+  }
+
+  addBoardToState(name, thumbnailPhoto) {
+    let { nextBoardId } = this.state;
+    nextBoardId += 1;
+    const newBoard = {
+      id: nextBoardId,
+      name,
+      thumbnailPhoto,
+    };
+    const { boards } = this.state;
+    this.setState({ boards: [...boards, newBoard], isCreateBoardModalOpen: false, nextBoardId });
   }
 
   async takePhoto() {
-      const photo = await takePhoto();
-      if (photo.length > 0) { await this.addImage(photo); }
+    const photo = await takePhoto();
+    if (photo.length > 0) { this.setState({ thumbnailPhoto: photo }); }
   }
 
   async selectFromCameraRoll() {
-      const photo = await selectFromCameraRoll();
-      if (photo.length > 0) { await this.addImage(photo); }
+    const photo = await selectFromCameraRoll();
+    if (photo.length > 0) { this.setState({ thumbnailPhoto: photo }); }
   }
 
   async addBoard(name) {
     const { thumbnailPhoto } = this.state;
-    const { addBoardToState } = this.props;
     if (thumbnailPhoto === '') {
       Alert.alert(
-        'A photo is is required!',
-        'You can add a photo by selecting the camera or albom icon',
+        'Photo need to be added.',
+        'You can add a photo by selecting the camera or album icon',
       );
     } else {
-      addBoardToState(name, thumbnailPhoto);
+      this.addBoardToState(name, thumbnailPhoto);
       this.setState({
-        isAddModalOpen: false,
+        isCreateBoardModalOpen: false,
         thumbnailPhoto: '',
       });
     }
   }
 
   render() {
-    const {boards, isCreateBoardModalOpen} = this.state;
+    const { boards, isCreateBoardModalOpen } = this.state;
     return (
       <View style={{ flex: 1 }}>
         <Toolbar
-            onAdd={ () => this.setState({ isCreateBoardModalOpen: true }) }
-            title ='Boards Overview'/>
+          onAdd={() => this.setState({ isCreateBoardModalOpen: true })}
+          title="Boards Overview"
+        />
         <BoardList
-            boards={boards} />
+          boards={boards}
+        />
         <CreateBoard
           isOpen={isCreateBoardModalOpen}
-          closeModal={() => this.setState({isCreateBoardModalOpen: false})}
+          closeModal={() => this.setState({ isCreateBoardModalOpen: false })}
           takePhoto={() => this.takePhoto()}
           selectFromCameraRoll={() => this.selectFromCameraRoll()}
-          onSubmit={(name) => this.addBoard(name)}/>
+          onSubmit={(name) => this.addBoard(name)}
+        />
       </View>
     );
   }
 }
-
-Boards.propTypes = {
-  addBoardToState: PropTypes.func.isRequired,
-};
 
 export default Boards;
