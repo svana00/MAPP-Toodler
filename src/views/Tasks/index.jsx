d2ed2b1011b4897c420f11fdcce8d077077e0899
svana00 <54863023+svana00@users.noreply.github.com>
2020-11-27 15:31:31 +0000

    fixed more linter errors

--- src/views/Tasks/index.jsx
+++ src/views/Tasks/index.jsx
@@ -1,166 +1,169 @@
 import React from 'react';
-import { View, Text, Alert } from 'react-native';
+import { View, Alert } from 'react-native';
 import TaskList from '../../components/TaskList';
 import AddTask from '../../components/AddTask';
 import Spinner from '../../components/Spinner';
-//import { getAllTasks, addImage, remove } from '../../services/fileService';
+// import { getAllTasks, addImage, remove } from '../../services/fileService';
 import TaskToolbar from '../../components/taskToolbar';
-import * as colors from '../../styles/colors';
-import { headings } from '../../styles/headings';
 import data from '../../resources/data.json';
-import Toolbar from '../../components/Toolbar';
-
 
 class Tasks extends React.Component {
-    static navigationOptions = {
-      title: 'Tasks'
-    }
-
-    state = {
-      //All tasks within the application directory
+  constructor(props) {
+    super(props);
+    this.state = {
+      // All tasks within the application directory
       tasks: [],
-      //just while testing
-      listId: "",
-      listName: "",
-      //selected tasks
+      // just while testing
+      listId: '',
+      listName: '',
+      // selected tasks
       selectedTasks: [],
       loadingTasks: true,
       isAddModalOpen: false,
-      currentId: "",
-      isBeingModified: false
-    }
-
-
-    async componentDidMount() {
-      const {navigation} = this.props;
-      const listId = navigation.getParam('listId', '');
-      const listName = navigation.getParam('listName', '');
-      await this.setState({listId, listName })
-      await this.__getItems();
-    }
-
-    async __getItems() {
-        this.setState({ loadingTasks: true});
-        const { listId} = this.state
-        const tasks = data.tasks.filter(task => task.listId == listId)
-        await this.setState({loadingTasks: false, tasks})
-    }
-
-    onTaskLongPress(id) {
-      const { selectedTasks } = this.state;
-      if (selectedTasks.indexOf(id) !== -1){
-        //The task is already in the list
-        this.setState({selectedTasks: selectedTasks.filter(task => task!=id)});
-      } else {
-        // Add a new image
-        this.setState({selectedTasks: [...selectedTasks, id]});
-      }
+      currentId: '',
+      isBeingModified: false,
+    };
   }
 
-    async deleteSelectedTasks() {
-      const {selectedTasks, tasks} = this.state;
-      this.setState({loadingTasks: true})
-      this.setState({
-        selectedTasks: [],
-        tasks: tasks.filter(task => selectedTasks.indexOf(task.id) === -1),
-        loadingTasks: false
-      })
+  async componentDidMount() {
+    const {navigation} = this.props;
+    const listId = navigation.getParam('listId', '');
+    const listName = navigation.getParam('listName', '');
+    await this.setState({ listId, listName })
+    await this.getItems();
+  }
 
+  onTaskLongPress(id) {
+    const { selectedTasks } = this.state;
+    if (selectedTasks.indexOf(id) !== -1) {
+      // The task is already in the list
+      this.setState({ selectedTasks: selectedTasks.filter((task) => task !== id) });
+    } else {
+      // Add a new image
+      this.setState({ selectedTasks: [...selectedTasks, id] });
     }
+  }
 
-    async makeTask(task) {
-      await this.addTask(task);
+  async setupModify(id) {
+    await this.setState({ currentId: id, isBeingModified: true, isAddModalOpen: true });
+  }
+
+  async getItems() {
+    this.setState({ loadingTasks: true });
+    const { listId } = this.state;
+    const tasks = data.tasks.filter((task) => task.listId === listId);
+    await this.setState({ loadingTasks: false, tasks });
+  }
+
+  async deleteSelectedTasks() {
+    const { selectedTasks, tasks } = this.state;
+    this.setState({ loadingTasks: true });
+    this.setState({
+      selectedTasks: [],
+      tasks: tasks.filter((task) => selectedTasks.indexOf(task.id) === -1),
+      loadingTasks: false,
+    });
+  }
+
+  async makeTask(task) {
+    await this.addTask(task);
+  }
+
+  async addTask(task) {
+    const { tasks, listId } = this.state;
+    const id = tasks.length + 1;
+    if (task.name.length === 0 || task.description.length === 0) {
+      Alert.alert(
+        'Blank fields',
+        'You can not have any blank fields, Please fill it all in',
+        [{ text: 'Understood' }]
+      );
+    } else {
+      const newTask = {id: id.toString(), name: task.name, description: task.description, isFinished: false, ListId: listId};
+      this.setState({tasks: [...tasks,newTask], isAddModalOpen: false});
     }
-    async addTask(task) {
-      this.setState({loadingImages: true})
-      const {tasks, listId } = this.state;
-      const id = tasks.length + 1;
-      if (task.name.length == 0 || task.description.length == 0){
-        Alert.alert(
-          'Blank fields',
-          'You can not have any blank fields, Please fill it all in',
-          [{text: 'Understood'}]
-        );
+  }
+
+  async flipFinished(id) {
+    const { tasks } = this.state;
+    for (var i in tasks) {
+      if (tasks[i].id == id) {
+        if (tasks[i].isFinished) {
+          tasks[i].isFinished = false;
+        }
+        else {
+          tasks[i].isFinished = true;
+        }
       }
-      else {
-        const newTask = {id: id.toString(), name: task.name, description: task.description, isFinished: false, ListId: listId};
-        this.setState({tasks: [...tasks,newTask], loadingImages: false, isAddModalOpen: false});
-      }
     }
+    await this.setState(tasks)
+  }
 
-    async flipFinished(id) {
-      const {tasks} = this.state;
+  async modify(task) {
+    const { tasks } = this.state;
+    if (task.name.length === 0 || task.description.length === 0) {
+      Alert.alert(
+        'Blank fields',
+        'You can not have any blank fields, Please fill it all in',
+        [{ text: 'Understood' }],
+      );
+    } else {
       for (var i in tasks) {
-        if (tasks[i].id == id) {
-          if (tasks[i].isFinished) {
-            tasks[i].isFinished = false;
-          }
-          else {
-            tasks[i].isFinished = true;
-          }
+        if (tasks[i].id == task.id) {
+          tasks[i].name = task.name;
+          tasks[i].description = task.description;
         }
       }
-      await this.setState(tasks)
+      await this.setState({
+        tasks, isAddModalOpen: false, isBeingModified: false, currentId: '',
+      });
     }
+  }
 
-    async setupModify(id) {
-      await this.setState({currentId: id, isBeingModified: true, isAddModalOpen: true })
-    }
-
-    async modify(task){
-      const {tasks} = this.state;
-      if (task.name.length == 0 || task.description.length == 0){
-        Alert.alert(
-          'Blank fields',
-          'You can not have any blank fields, Please fill it all in',
-          [{text: 'Understood'}]
-        );
-      }
-      else {
-        for (var i in tasks) {
-          if (tasks[i].id == task.id) {
-            tasks[i].name = task.name;
-            tasks[i].description = task.description;
-          }
-        }
-        await this.setState({tasks:tasks, isAddModalOpen: false, isBeingModified: false, currentId:""})
-        }
-    }
-
-      render() {
-        const { currentId, selectedTasks, loadingTasks, tasks, isAddModalOpen, listName,isBeingModified } = this.state;
-        return (
-          <View style={{ flex:1 }}>
-              <TaskToolbar
-                  hasSelectedTasks={ selectedTasks.length > 0 }
-                  onAdd={ () => this.setState({ isAddModalOpen: true }) }
-                  onRemove={ () => this.deleteSelectedTasks() }
-                  listName = { listName }/>
-              {
-                  loadingTasks
-                  ?
-                  <Spinner />
-                  :
-                  <>
-                      <TaskList
-                          tasks = { tasks }
-                          selectedTasks = { selectedTasks }
-                          onLongPress = {id => this.onTaskLongPress(id)}
-                          flipFinished = {async (id) => this.flipFinished(id)}
-                          onModify = {id => this.setupModify(id)}
-                          />
-                  </>
-               }
-               <AddTask
-                  id= {currentId}
-                  isOpen={isAddModalOpen}
-                  closeModal = {() => this.setState({isAddModalOpen: false})}
-                  addTask = {task => this.makeTask(task)}
-                  modify = {isBeingModified}
-                  onModify = {id => this.modify(id)}/>
-          </View>
-        );
-    }
+  render() {
+    const {
+      currentId,
+      selectedTasks,
+      loadingTasks,
+      tasks,
+      isAddModalOpen,
+      listName,
+      isBeingModified,
+    } = this.state;
+    return (
+      <View style={{ flex: 1 }}>
+        <TaskToolbar
+          hasSelectedTasks={selectedTasks.length > 0}
+          onAdd={() => this.setState({ isAddModalOpen: true })}
+          onRemove={() => this.deleteSelectedTasks()}
+          listName={listName}
+        />
+        {
+          loadingTasks
+            ? <Spinner />
+            : (
+              <>
+                <TaskList
+                  tasks={tasks}
+                  selectedTasks={selectedTasks}
+                  onLongPress={(id) => this.onTaskLongPress(id)}
+                  flipFinished={async (id) => this.flipFinished(id)}
+                  onModify={(id) => this.setupModify(id)}
+                />
+              </>
+            )
+}
+        <AddTask
+          id={currentId}
+          isOpen={isAddModalOpen}
+          closeModal={() => this.setState({ isAddModalOpen: false })}
+          addTask={(task) => this.makeTask(task)}
+          modify={isBeingModified}
+          onModify={(id) => this.modify(id)}
+        />
+      </View>
+    );
+  }
 }
 
 export default Tasks;
